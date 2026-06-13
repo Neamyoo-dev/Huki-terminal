5d9f2757c9bbbe3073066bfe43b22d5a157f4514
oneachina <ruiruixi@hotmail.com>
2026-06-13 23:18:33 +0800

    refactor(thread): remove dead _read_output method
    - _read_output() referenced self._process which was never assigned (always None)
    
    - Also remove unused self._process = None from __init__

--- utils/thread_utils.py
+++ utils/thread_utils.py
@@ -9,7 +9,6 @@ class ThreadUtils(QThread):
     def __init__(self):
         super(ThreadUtils, self).__init__()
         self.time_number = 0
-        self._process = None
 
     def run(self):
         while True:
@@ -18,10 +17,3 @@ class ThreadUtils(QThread):
             h, m = divmod(m, 60)
             self.trigger.emit(str("%02d:%02d:%02d" % (h, m, s)))
             time.sleep(0.01)
-
-    def _read_output(self):
-        if self._process:
-            for line in self._process.stdout:
-                self.trigger.emit(line.strip())
-            self._process.stdout.close()
-            self._process.wait()
