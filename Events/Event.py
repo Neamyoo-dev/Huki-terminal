788417a1e05e4bacc76dac5ac8c8a98437046ef6
oneachina <ruiruixi@hotmail.com>
2026-06-14 00:05:12 +0800

    refactor(event): replace inheritance with EventMixin
    REFACTOR-R4: Convert Event(Ui_MainWindow) class into EventMixin mixin class
    
      - text_edit widget creation and styling moved to MainForm.__init__
    
      - warning()/info()/error()/print() become mixin methods operating on self.text_edit
    
      - Add from __future__ import annotations, thorough type hints
    
      - Fix error() to use str() on non-string values (was crashing on Exception)

--- Events/Event.py
+++ Events/Event.py
@@ -1,53 +1,41 @@
-from PyQt5 import QtCore
+from __future__ import annotations
 
-from Events.CustomPlainTextEdit import *
-from ui import Ui_MainWindow
+from PyQt5.QtGui import QTextCursor, QTextCharFormat, QBrush, QColor
 
 
-class Event(Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.text_edit = CustomPlainTextEdit(self.frame)
-
-        self.text_edit.setGeometry(QtCore.QRect(0, 50, 1521, 671))
-        self.text_edit.setStyleSheet("QPlainTextEdit#plainTextEdit"
-                                     "{background-color: rgb(12, 12, 12);font: 13pt \"Cascadia Code\";color:rgb(255, "
-                                     "255, 255);"
-                                     "border-radius:13px;}")
-        self.text_edit.setObjectName("plainTextEdit")
-
-    def warning(self, info: str | list | tuple):
+class EventMixin:
+    def warning(self, info: str | list | tuple) -> None:
         cursor = self.text_edit.textCursor()
-        cursor.movePosition(QtGui.QTextCursor.End)
+        cursor.movePosition(QTextCursor.End)
 
-        color_format = QtGui.QTextCharFormat()
-        color_format.setForeground(QtGui.QBrush(QtGui.QColor("yellow")))
+        color_format = QTextCharFormat()
+        color_format.setForeground(QBrush(QColor("yellow")))
         cursor.setCharFormat(color_format)
 
         cursor.insertText("\n" + "".join(info))
 
-        cursor.setCharFormat(QtGui.QTextCharFormat())
+        cursor.setCharFormat(QTextCharFormat())
         self.text_edit.setTextCursor(cursor)
 
-    def info(self, info: str | list | tuple):
+    def info(self, info: str | list | tuple) -> None:
         cursor = self.text_edit.textCursor()
-        cursor.movePosition(QtGui.QTextCursor.End)
+        cursor.movePosition(QTextCursor.End)
 
-        color_format = QtGui.QTextCharFormat()
-        color_format.setForeground(QtGui.QBrush(QtGui.QColor("blue")))
+        color_format = QTextCharFormat()
+        color_format.setForeground(QBrush(QColor("blue")))
         cursor.setCharFormat(color_format)
 
         cursor.insertText("\n" + "".join(info))
 
-        cursor.setCharFormat(QtGui.QTextCharFormat())
+        cursor.setCharFormat(QTextCharFormat())
         self.text_edit.setTextCursor(cursor)
 
-    def error(self, info: str | list | tuple | Exception):
+    def error(self, info: str | list | tuple | Exception) -> None:
         cursor = self.text_edit.textCursor()
-        cursor.movePosition(QtGui.QTextCursor.End)
+        cursor.movePosition(QTextCursor.End)
 
-        color_format = QtGui.QTextCharFormat()
-        color_format.setForeground(QtGui.QBrush(QtGui.QColor("red")))
+        color_format = QTextCharFormat()
+        color_format.setForeground(QBrush(QColor("red")))
         cursor.setCharFormat(color_format)
 
         if isinstance(info, (list, tuple)):
@@ -55,9 +43,9 @@ class Event(Ui_MainWindow):
         else:
             cursor.insertText("\n" + str(info))
 
-        cursor.setCharFormat(QtGui.QTextCharFormat())
+        cursor.setCharFormat(QTextCharFormat())
         self.text_edit.setTextCursor(cursor)
 
-    def print(self, value: str | list | tuple, sep="", end=""):
+    def print(self, value: str | list | tuple, sep: str = "", end: str = "") -> None:
         text = sep.join(value) if isinstance(value, (list, tuple)) else str(value)
         self.text_edit.appendPlainText(text + end)
